c87d58316ee3a33fad880e80deb5e9128619f21e
Zer0 <zero30paul@yandex.ru>
2026-07-21 10:41:52 +0300

    Update before_script.msvc.sh

--- CI/before_script.msvc.sh
+++ CI/before_script.msvc.sh
@@ -917,11 +917,12 @@ printf "Qt ${QT_VER}... "
 		echo "  Ensuring setuptools is installed in virtualenv..."
 		run_cmd "aqt-venv/${VENV_BIN_DIR}/python" -m pip install --upgrade pip setuptools wheel
 
-		# Check version
-		aqt-venv/${VENV_BIN_DIR}/pip list | grep 'aqtinstall\s*1.1.3' || [ $? -ne 0 ]
-		if [ $? -eq 0 ]; then
-			echo "  Installing aqt wheel into virtualenv..."
-			run_cmd "aqt-venv/${VENV_BIN_DIR}/pip" install aqtinstall==1.1.3
+		# aqtinstall 1.1.3 is too old for current Python/Qt mirrors.
+		# Pin a modern, reproducible version that supports Python 3.12+.
+		if ! "aqt-venv/${VENV_BIN_DIR}/python" -m pip show aqtinstall 2>/dev/null \
+			| grep -q '^Version: 3.3.0$'; then
+			echo "  Installing aqtinstall 3.3.0 into virtualenv..."
+			run_cmd "aqt-venv/${VENV_BIN_DIR}/python" -m pip install --upgrade --force-reinstall aqtinstall==3.3.0
 		fi
 		popd > /dev/null
 
@@ -930,7 +931,10 @@ printf "Qt ${QT_VER}... "
 		mkdir Qt
 		cd Qt
 
-		run_cmd "${DEPS}/aqt-venv/${VENV_BIN_DIR}/aqt" install ${QT_VER} windows desktop "win${BITS}_msvc${QT_MSVC_YEAR}${SUFFIX}"
+		# Modern aqt syntax: host target version architecture.
+		run_cmd "${DEPS}/aqt-venv/${VENV_BIN_DIR}/python" -m aqt install-qt \
+			windows desktop "${QT_VER}" "win${BITS}_msvc${QT_MSVC_YEAR}${SUFFIX}" \
+			--outputdir "$(real_pwd)"
 
 		printf "  Cleaning up extraneous data... "
 		rm -rf Qt/{aqtinstall.log,Tools}
