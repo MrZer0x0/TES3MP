d9e6fc09d6a653e4871a1bedc4b0da2c136f17b7
Zer0 <zero30paul@yandex.ru>
2026-07-22 21:58:12 +0300

    fix

--- apps/openmw/mwgui/hud.hpp
+++ apps/openmw/mwgui/hud.hpp
@@ -121,6 +121,7 @@ namespace MWGui
             float idleTimer = 0.f;
             float alpha = 1.f;
             bool initialized = false;
+            bool resourceVisible = true;
         };
 
         AutoHideBarState mHealthBarState;
@@ -129,8 +130,11 @@ namespace MWGui
         bool mHmsBaseVisible;
 
         void registerBarChange(AutoHideBarState& state, int current, int modified);
-        void updateAutoHideBar(MyGUI::Widget* frame, AutoHideBarState& state, float dt, bool forceVisible);
+        void updateAutoHideBar(MyGUI::Widget* frame, MyGUI::Widget* bar, MyGUI::TextBox* text,
+                               AutoHideBarState& state, float dt, bool forceVisible, bool keepFrameVisible);
         void applyBarAlpha(MyGUI::Widget* widget, float alpha);
+        void applyResourceBarVisuals(MyGUI::Widget* frame, MyGUI::Widget* bar, MyGUI::TextBox* text,
+                                     AutoHideBarState& state, bool visible, float alpha, bool keepFrameVisible);
 
         void onWorldClicked(MyGUI::Widget* _sender);
         void onWorldMouseOver(MyGUI::Widget* _sender, int x, int y);
